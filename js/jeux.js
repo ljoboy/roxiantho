0ceb501581fb53618b4e1a16d0a01d0253b647b2
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-10-10 23:17:02 +0200

    [UPDATE] finish with js

--- js/jeux.js
+++ js/jeux.js
@@ -1,33 +1,49 @@
 var div = [$("#1"), $("#2"), $("#3"), $("#4")];
-var rep;
+var jo, rep;
 var colors = ["red", "brown", "cyan", "blue", "green", "violet", "grey"];
 
 function color() {
     for (let i = 0; i < div.length; i++) {
         const el = div[i];
-        el.toggleClass(colors[Math.floor(Math.random() * colors.length)]);
+        el.attr("class", "c2 " + colors[Math.floor(Math.random() * colors.length)]);
     }
 }
 color();
 $("button").click(() => {
-    rep = div[Math.floor(Math.random() * div.length)];
+
+    chWord = setInterval(() => {
+        color();
+        rep = Math.floor(Math.random() * div.length);
+        for (let i = 0; i < div.length; i++) {
+            el = div[i];
+            if (rep == i) {
+                el.text("Gagnant");
+            } else {
+                el.text("Trouve moi");
+            }
+        }
+    }, 500);
     setTimeout(() => {
-        rep.html("Gagnant");
-    }, 300);
-    chColor = setInterval(color, 250);
-    setTimeout(() => {
-        rep.html("Trouve moi");
-    }, 1000);
-    setTimeout(() => {
-        clearInterval(chColor);
+        blackify();
+        jo = div[rep];
+        clearInterval(chWord);
     }, 3000);
+
 });
 $(".parent").click(
     (e) => {
-        if (e.target.id === rep[0].id) {
+        if (e.target.id === jo[0].id) {
             swal("Gagner", "Réponse correcte", "success");
         } else {
             swal("Perdu", "Réponse Incorrecte", "error");
         }
     }
 );
+
+function blackify() {
+    for (let i = 0; i < div.length; i++) {
+        const el = div[i];
+        el.attr("class", "c2 black txt");
+        el.html(i + 1);
+    }
+}
