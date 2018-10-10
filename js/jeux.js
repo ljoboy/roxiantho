9792d0297c888060fc40cf819692e4547371310b
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-10-10 13:15:52 +0200

    [UPDATE] add button click func

--- js/jeux.js
+++ js/jeux.js
@@ -1,6 +1,16 @@
 var div = [$("#1"), $("#2"), $("#3"), $("#4")];
-div[Math.floor(Math.random() * div.length)].html("Gagnant");
 
+$("button").click(() => {
+    var rep = div[Math.floor(Math.random() * div.length)];
+    setTimeout(() => {
+        rep.html("Gagnant");
+    }, 300);
+    setTimeout(() => {
+        rep.html("Trouve moi");
+    }, 1700);
+    clearInterval(rep);
+
+});
 $(".parent").click(
     (e) => {
 
