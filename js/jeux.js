8b89dab22d59a75683c32825a4e3f684a3e2669d
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-10-12 13:26:06 +0200

    [UPDATE] add fadeIn effect

--- js/jeux.js
+++ js/jeux.js
@@ -1,73 +1,78 @@
-var div = [$('#1'), $('#2'), $('#3'), $('#4')];
-var jo, rep, lance = 0;
-var colors = ["red", "brown", "cyan", "blue", "green", "violet", "grey"];
+var div = [$('#1'), $('#2'), $('#3'), $('#4')]
+var jo, rep, lance = 0
+var colors = ['red', 'brown', 'cyan', 'blue', 'green', 'violet', 'grey']
 
 function color() {
     for (let i = 0; i < div.length; i++) {
-        const el = div[i];
-        el.attr("class", "c2 txt bordr " + colors[Math.floor(Math.random() * colors.length)]);
-        div[0].text("trouve moi");
-        div[1].text("trouve moi");
-        div[2].text("trouve moi");
-        div[3].text("trouve moi");
+        const el = div[i]
+        el.attr(
+            'class',
+            'c2 txt bordr ' + colors[Math.floor(Math.random() * colors.length)]
+        )
+        div[0].text('trouve moi')
+        div[1].text('trouve moi')
+        div[2].text('trouve moi')
+        div[3].text('trouve moi')
     }
 }
-color();
-$("button").click((e) => {
-    e.preventDefault();
+color()
+$('button').click(e => {
+    e.preventDefault()
     if (lance === 0) {
         var chWord = setInterval(() => {
-            color();
-            rep = Math.floor(Math.random() * div.length);
-            div[rep].text("gagnant");
-        }, 500);
+            color()
+            rep = Math.floor(Math.random() * div.length)
+            div[rep].text('gagnant')
+        }, 250)
         setTimeout(() => {
-            blackify();
-            jo = div[rep];
-            clearInterval(chWord);
-        }, 3000);
-        lance = 1;
+            blackify()
+            jo = div[rep]
+            clearInterval(chWord)
+        }, 6000)
+        lance = 1
     }
-
-
-});
-$(".parent").click(
-    (e) => {
-        if (jo) {
-            if (e.target.id === jo[0].id) {
-                swal("Gagner", "Réponse correcte", "success");
-                lance = 0;
-                jo = false;
-                color();
-            } else {
-                swal("Perdu", "Réponse Incorrecte", "error");
-            }
+})
+$('.parent').click(e => {
+    if (jo) {
+        if (e.target.id === jo[0].id) {
+            swal('Gagner', 'Réponse correcte', 'success')
+            lance = 0
+            jo = false
+            color()
         } else {
-            swal("Attention", "Veuillez cliquer sur play pour commencer", "warning");
+            swal('Perdu', 'Réponse Incorrecte', 'error')
         }
+    } else {
+        swal('Attention', 'Veuillez cliquer sur play pour commencer', 'warning')
     }
-);
+})
 
 function blackify() {
     for (let i = 0; i < div.length; i++) {
-        const el = div[i];
-        el.attr("class", "c2 black txt border");
-        el.html(i + 1);
+        const el = div[i]
+        el.attr('class', 'c2 black txt border')
+        el.html(i + 1)
     }
 }
 
 function aide() {
-    var carree = $("<div class='carree'></div>");
-    var noir = $("<div class='noir'></div>");
-    var p = $("<p class='p'></p>");
-    p.html("Ce jeux consiste &agrave; retrouver le dernier emplacement du mot <b>Gagnant</b>.<br/>Jeux concu et " +
-        "realiser avec l'aide de <b>Junior Yombo Sosse</b>");
-    carree.prepend("<div class='fermer darkred' onclick='fermer()'>&#10060;</div>");
-    carree.append(p);
-    $("body").prepend(noir, carree);
+    var carree = $("<div class='carree'></div>")
+    var noir = $("<div class='noir'></div>")
+    var p = $("<p class='p'></p>")
+    p.html(
+        'Ce jeux consiste &agrave; retrouver le dernier emplacement du mot <b>Gagnant</b>.<br/>Jeux concu et ' +
+        "realiser avec l'aide de <b>Junior Yombo Sosse</b>"
+    )
+    carree.prepend(
+        "<div class='fermer darkred' onclick='fermer()'>&#10060;</div>"
+    )
+    carree.append(p)
+    carree.fadeIn(1500)
+    noir.fadeIn(1500)
+    $('body').prepend(noir, carree)
 }
 
 function fermer() {
-    $(".noir").remove();
-    $(".carree").remove();
+    $('.noir').remove()
+    $('.carree').remove()
 }
