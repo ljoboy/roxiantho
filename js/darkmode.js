37f880e386d1e8e77baf9eb3a24737d59a6ad499
Chrinovic MM <20mm364@esisalama.org>
2022-09-10 14:41:59 +0200

    Ajout du JS pour le mode sombre

--- js/darkmode.js
+++ js/darkmode.js
@@ -12,3 +12,32 @@ function darkmode(){
         darkbutton.value="Mode Nuit"
     }
 }
+
+
+let body = document.querySelector('body');
+        let toggle = document.querySelector(".toggle");
+
+        let stockMode = localStorage.getItem("mode");
+        if(stockMode && stockMode === "dark"){
+            body.classList.add("dark");
+            toggle.classList.add("active");
+        }
+        
+
+        toggle.addEventListener("click", ()=>{
+            if(!body.classList.contains('dark')){
+                body.classList.toggle("dark");
+                resultat.style.backgroundColor = "#301c50"
+            }else{
+                body.classList.remove('dark')
+                resultat.style.backgroundColor = "#fff"
+            }
+
+            if(!body.classList.contains("dark")){
+                return localStorage.setItem("mode", "light")
+            }
+            localStorage.setItem("mode", "dark");
+        })
+        toggle.addEventListener("click", () =>
+            toggle.classList.toggle("active")
+        )
