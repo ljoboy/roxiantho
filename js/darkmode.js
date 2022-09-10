18b06b5f667ea0f1e811ff19078f4011c24eb5c4
Chrinovic MM <20mm364@esisalama.org>
2022-09-10 14:46:51 +0200

    Ajout du style du body pour les modes (sombre et clair)

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,43 +1,29 @@
-var darkbutton = document.getElementById("darkbutton")
-var corp = document.getElementById("body")
-var titre = document.getElementById("titre")
-
-function darkmode(){
-    corp.classList.toggle("darkmode")
-    titre.classList.toggle("white")
-    if (darkbutton.value=="Mode Nuit") {
-        darkbutton.value="Mode Jour"
-    }
-    else{
-        darkbutton.value="Mode Nuit"
-    }
-}
 
 
 let body = document.querySelector('body');
-        let toggle = document.querySelector(".toggle");
+let toggle = document.querySelector(".toggle");
 
-        let stockMode = localStorage.getItem("mode");
-        if(stockMode && stockMode === "dark"){
-            body.classList.add("dark");
-            toggle.classList.add("active");
-        }
+let stockMode = localStorage.getItem("mode");
+
+if(stockMode && stockMode === "dark"){
+    body.classList.add("dark");
+    toggle.classList.add("active");
+}
         
 
-        toggle.addEventListener("click", ()=>{
-            if(!body.classList.contains('dark')){
-                body.classList.toggle("dark");
-                resultat.style.backgroundColor = "#301c50"
-            }else{
-                body.classList.remove('dark')
-                resultat.style.backgroundColor = "#fff"
-            }
+toggle.addEventListener("click", ()=>{
+    if(!body.classList.contains('dark')){
+            body.classList.toggle("dark");
+    }else{
+            body.classList.remove('dark')
+            resultat.style.backgroundColor = "#fff"
+    }
 
-            if(!body.classList.contains("dark")){
-                return localStorage.setItem("mode", "light")
-            }
-            localStorage.setItem("mode", "dark");
-        })
-        toggle.addEventListener("click", () =>
-            toggle.classList.toggle("active")
-        )
+    if(!body.classList.contains("dark")){
+        return localStorage.setItem("mode", "light")
+    }
+    localStorage.setItem("mode", "dark");
+});
+toggle.addEventListener("click", () =>
+    toggle.classList.toggle("active")
+);
